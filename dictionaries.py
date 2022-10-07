8b5cbc350ac11af9ed19cb66d1205a6e97c214a3
DiegoFuentesDev <al229847@edu.uaa.mx>
2022-10-07 13:39:49 -0500

    Adding dictionaries and how to print them

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+dictionaryNames = {
+    "name" : "John Smith",
+    "age" : 25,
+    "occupation" : "Lawyer"
+}
+
+print("Dictionary:",dictionaryNames)
+print("Only the name",dictionaryNames["name"])
