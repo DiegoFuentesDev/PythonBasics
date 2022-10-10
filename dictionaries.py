39123e92de2d47f9843d825a835ddba91d75c55d
DiegoFuentesDev <al229847@edu.uaa.mx>
2022-10-10 07:36:50 -0500

    Dictionaries version 1

--- dictionaries.py
+++ dictionaries.py
@@ -1,10 +1,24 @@
 dictionaryNames = {
     "name" : "John Smith",
     "age" : 25,
-    "occupation" : "Lawyer"
+    "occupation" : "Lawyer",
+    "hobbie" : "Cinema",
+    "Game Console" : "Xbox"
 }
 
 print("Dictionary:",dictionaryNames)
 print("Only the name",dictionaryNames["name"])
 dictionaryNames["name"] = "Kevin Potter"
 print("Name Changed:",dictionaryNames)
+print("Number of elements in the dictionary:",len(dictionaryNames))
+dictionaryNames["Gender"] = "Male"
+dictionaryNames.pop("age")
+print("The dictionary after pop the element 'age'",dictionaryNames)
+dictionaryNames.popitem()
+print("The dictionary after use the .popitem in the dictionary, that remove the last element of it:", dictionaryNames)
+dictionaryCopy = dictionaryNames.copy()
+dictionaryDelete = dict(dictionaryCopy)
+dictionaryDelete.clear()
+print("Original", dictionaryNames)
+print("Copy", dictionaryCopy)
+print("Deleted", dictionaryDelete)
